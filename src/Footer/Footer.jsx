c82c242f7733e70325ea51e2e16c331f72550fd8
Jinath Yasmin Laskar <60038325+jinath123-yasmin@users.noreply.github.com>
2023-08-08 13:40:33 +0530

    Update Footer.jsx

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,46 +1,52 @@
 import "./Footer.css";
-import original from "../images/original.png";
-import returnButton from "../images/return-button.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return(
-        <div className="footer-wrapper">
-            <div className="content-wrapper">
-                <div>
-                    <h5>ONLINE SHOPPING</h5>
-                    <div>Men</div>
-                    <div>Women</div>
-                    <div>Kids</div>
-                    <div>Home & Living</div>
-                    <div>Gift Cards</div>
-                    <div>Myntra Insider</div>
-                </div>
-                <div>
-                    <div className="guarantee-wrapper">
-                        <img src={original} alt="original-img"/>
-                        <div>
-                            <div><span className="original">100% ORIGINAL</span> guarantee for</div>
-                            <div>all products at myntra.com</div>
-                        </div>
-                    </div>
-                    <div className="return-wrapper">
-                        <img src={returnButton} alt="return-img"/>
-                        <div>
-                            <div><span className="return">Return within 14days</span> of</div>
-                            <div>receiving your order</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="copy-right-wrapper">
-                <div>
-                    <div>In case of any concern, <span>Contact Us</span></div>                    
-                </div>
-                <div>
-                    <div>&copy; 2023 www.myntra.com. All rights reserved.</div>
-                </div>
-            </div>
+        <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h3>About Us</h3>
+          <p>Discover the latest trends in fashion and explore a wide range of products at Myntra.</p>
         </div>
+        <div className="footer-section">
+          <h3>Customer Service</h3>
+          <ul>
+            <li>Contact Us</li>
+            <li>FAQs</li>
+            <li>Shipping & Returns</li>
+          </ul>
+        </div>
+        <div className="footer-section">
+        
+          <h3>Follow Us</h3>
+          <ul className="social-icons">
+            <li>
+              <a href="https://m.facebook.com/">
+                <FontAwesomeIcon icon={faFacebookF} />
+                <span className="social-label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/">
+                <FontAwesomeIcon icon={faInstagram} />
+                <span className="social-label">Instagram</span>
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/">
+                <FontAwesomeIcon icon={faYoutube} />
+                <span className="social-label">YouTube</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} Myntra. All rights reserved.</p>
+      </div>
+    </footer>
     )
 }
 export default Footer;
